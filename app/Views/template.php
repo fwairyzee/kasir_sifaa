9075f8b5f5f5b65d5fa45eb86aea558cd157fa39
fwairyzee <161412296+fwairyzee@users.noreply.github.com>
2024-02-28 08:14:26 +0700

    changes

--- app/Views/template.php
+++ app/Views/template.php
@@ -165,7 +165,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
             <a href="index3.html" class="brand-link">
-                <img src="<?= base_url('AdminLTE') ?>/dist/img/pvnlogo.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <img src="<?= base_url('AdminLTE') ?>/dist/img/zoyalogo.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">Zoya.Hijab</span>
             </a>
 
@@ -174,7 +174,7 @@
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
 
                     <div class="info">
-                        <a href="#" class="d-block">Lilyanaa</a>
+                        <a href="#" class="d-block">SifaFauziah</a>
                     </div>
                 </div>
 
